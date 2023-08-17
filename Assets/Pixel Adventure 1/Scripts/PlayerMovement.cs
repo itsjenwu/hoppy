dcb5597f1b8c90a0d1c496ecad7a85dea0919b86
jen <bagnut@Aruns-MacBook-Pro.local>
2023-08-16 21:08:18 -0400

    create jump, fall, run animations & disabled double jump

--- Assets/Pixel Adventure 1/Scripts/PlayerMovement.cs
+++ Assets/Pixel Adventure 1/Scripts/PlayerMovement.cs
@@ -7,10 +7,15 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private Animator anim;
+    private BoxCollider2D coll;
 
+    [SerializeField] private LayerMask jumpableGround;
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpSpeed = 14f;
+
+    private enum MovementState { idle, running, jumping, falling };
+    private MovementState state = MovementState.idle;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,7 +32,8 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(0, jumpSpeed);
         }
@@ -38,20 +44,37 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        //running animation
         if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
 
         }
+
+        //jumping animation - higher priority
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
